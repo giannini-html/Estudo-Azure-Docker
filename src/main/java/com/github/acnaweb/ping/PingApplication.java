5ade08315f1a85534eec2f8b89a1cfe6662f8729
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-17 21:02:33 -0300

    fix

--- src/main/java/com/github/acnaweb/ping/PingApplication.java
+++ src/main/java/com/github/acnaweb/ping/PingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class PingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PingApplication.class, args);
